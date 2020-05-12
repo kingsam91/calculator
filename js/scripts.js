abfd2bcd825a888c42468aa49fd3e432f1cab591
kingsam91 <lambetgm@gmail.com>
2020-05-12 05:05:38 +0300

    add initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = functionhttps://jsfiddle.net/#(number1, number2) {
+return number1 + number2
+}
+ var number1 = parseInt(prompt('enter number'));
+ var number2 = parseInt(prompt('enter another number'));
+ var result = add(number1, number2);
+ alert(result);
